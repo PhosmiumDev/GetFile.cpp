6ee4024bf04f728d12700b5222e77d99e4017719
Phosmium <bubatzweh@protonmail.com>
2023-11-20 22:35:14 +0100

    GetFile
    In my cpp program I created the functions: "WriteFileMode" and "ReadFileMode". Before you read/write from/in a file you can use these functions so its easier for you. And I made it possible to save the whole file in 1 string.

--- Test.cpp
+++ Test.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 
 bool worked = false;
+std::string buffer;
+std::string settings;
 
 std::fstream GetFile(std::string FileName)
 {
@@ -12,17 +14,35 @@ std::fstream GetFile(std::string FileName)
     return file;
 }
 
+void WriteFileMode(std::fstream& file,std::string FileName)//before you write inside a file you should run this function
+{
+    if (file.is_open())
+    {
+        file.close();
+    }
+    file.open(FileName,std::ios::out);
+}
+
+void ReadFileMode(std::fstream& file,std::string FileName)//before you read inside a file you should run this function
+{
+    if (file.is_open())
+    {
+        file.close();
+    }
+    file.open(FileName,std::ios::in);
+}
+
 int main()
 {
     std::string FileName = "settings.txt";
     std::fstream file = GetFile(FileName);
-    std::string stuff;
-    file.open(FileName,std::ios::out);
-    file.write("abc", 3);
-    file.close();
-    file.seekg(0);
-    file.open(FileName,std::ios::in);
-    std::getline(file, stuff);
-    file.close();
-    std::cout << "stuff: " << stuff << '\n';
+    ReadFileMode(file,FileName);
+
+    std::getline(file, buffer);
+
+    while (std::getline(file, buffer))
+    {
+        settings.append(buffer+'\n');
+    }
+    std::cout<< settings << '\n';
 }
